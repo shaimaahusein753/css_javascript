3e3e013ede19e42771bfb011defd01cf166481b4
shaimaahusein753 <muhammad.usman@funavy.com>
2023-06-10 19:47:06 +0500

    php composeradded

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-M3-Challenge-HW',
+        'version' => 'dev-M3-Challenge-HW',
+        'reference' => '8c879fbda19bfcf65e46ff4e8b126f7e1413bae6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-M3-Challenge-HW',
+            'version' => 'dev-M3-Challenge-HW',
+            'reference' => '8c879fbda19bfcf65e46ff4e8b126f7e1413bae6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
